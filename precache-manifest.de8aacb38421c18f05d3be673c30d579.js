7fa10e4354488208a07c701fa3e9c0e70b7c5b15
Oscar <oscarbaden@gmail.com>
2021-04-01 01:41:40 +1300

    Updates

---
+++ precache-manifest.de8aacb38421c18f05d3be673c30d579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a2a30680ed27b616171e9857324c85",
+    "url": "/Sudoku-Solver/index.html"
+  },
+  {
+    "revision": "86f1b1a78d5b7509cd88",
+    "url": "/Sudoku-Solver/static/css/main.423f87af.chunk.css"
+  },
+  {
+    "revision": "986f32c648a27c2ed17d",
+    "url": "/Sudoku-Solver/static/js/2.a2e91b9c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Sudoku-Solver/static/js/2.a2e91b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f1b1a78d5b7509cd88",
+    "url": "/Sudoku-Solver/static/js/main.50a6b189.chunk.js"
+  },
+  {
+    "revision": "ea83f876e1cc12d211d0",
+    "url": "/Sudoku-Solver/static/js/runtime-main.51cb6dc7.js"
+  }
+]);
